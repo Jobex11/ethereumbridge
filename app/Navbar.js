4be9eb68e7b836bad0cfb093b16b5172e6b0a68f
jobex11 <joblawal33@gmail.com>
2024-09-06 16:41:36 +0100

    added nav

--- app/Navbar.js
+++ app/Navbar.js
@@ -1,11 +1,21 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import SearchIcon from "@mui/icons-material/Search";
 import ConnectWallet from "./components/ConnectWallet";
 import CryptoKingIcon from "./components/CryptoKingIcon";
+import { IconButton } from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu"; // Menu Icon for opening the nav bar
+import CloseIcon from "@mui/icons-material/Close"; // Optional: Close icon to close the nav bar
+import Link from "next/link";
 
 import WalletIcon from "@mui/icons-material/Wallet";
 const Navbar = ({ connectWallet, account }) => {
+  const [open, setOpen] = useState(false);
+
+  const toggleNav = () => {
+    setOpen(!open);
+  };
+
   return (
     <div className="flex md:flex-row flex-col-reverse justify-between mb-[35px] gap-6">
       <div className="lg:flex-1 flex flex-row max-w-[458px] py-2 pl-4 pr-2 h-[52px] bg-[#1c1c24] rounded-[100px]">
@@ -40,6 +50,42 @@ const Navbar = ({ connectWallet, account }) => {
         <h3 className="text-2xl text-main">
           Eth-<span className="text-gray-200">Rune</span> Gateway
         </h3>
+      </div>
+      {/* nav bar */}
+      <div className="md:hidden">
+        {/* Menu Icon to open the full-screen navigation */}
+        <IconButton onClick={toggleNav}>
+          <MenuIcon fontSize="large" className="text-main" /> {/* Menu Icon */}
+        </IconButton>
+
+        {/* Full-Screen Navigation */}
+        {open && (
+          <div className="fixed inset-0 bg-black bg-opacity-90 z-50 flex flex-col items-center justify-center w-screen h-screen">
+            {/* Close Icon */}
+            <IconButton
+              className="absolute top-4 right-4 text-main"
+              onClick={toggleNav}
+            >
+              <CloseIcon fontSize="large" />
+            </IconButton>
+
+            {/* Navigation Links */}
+            <nav className="flex flex-col space-y-8 text-center">
+              <Link href="/" className="text-white text-3xl ">
+                Home
+              </Link>
+              <Link href="#about" className="text-white text-3xl ">
+                View Transactions
+              </Link>
+              <Link href="#services" className="text-white text-3xl ">
+                Get Info
+              </Link>
+              <Link href="#contact" className="text-white text-3xl ">
+                Contact Us
+              </Link>
+            </nav>
+          </div>
+        )}
       </div>
     </div>
   );
